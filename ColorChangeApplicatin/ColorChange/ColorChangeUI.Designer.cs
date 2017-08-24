5f87f3f4a31e16950ca714ab58d173ca82127506
Saurove Dash <royalcse2752@gmail.com>
2017-08-24 00:43:10 -0800

    Color Changing Application
    It is a destop Application.

---
+++ ColorChangeApplicatin/ColorChange/ColorChangeUI.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ColorChange
+{
+    partial class ColorChangeUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.blueRadioButton = new System.Windows.Forms.RadioButton();
+            this.redRadioButton = new System.Windows.Forms.RadioButton();
+            this.greenRadioButton = new System.Windows.Forms.RadioButton();
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // blueRadioButton
+            // 
+            this.blueRadioButton.AutoSize = true;
+            this.blueRadioButton.Location = new System.Drawing.Point(39, 67);
+            this.blueRadioButton.Name = "blueRadioButton";
+            this.blueRadioButton.Size = new System.Drawing.Size(46, 17);
+            this.blueRadioButton.TabIndex = 0;
+            this.blueRadioButton.TabStop = true;
+            this.blueRadioButton.Text = "Blue";
+            this.blueRadioButton.UseVisualStyleBackColor = true;
+            this.blueRadioButton.CheckedChanged += new System.EventHandler(this.blueRadioButton_CheckedChanged);
+            // 
+            // redRadioButton
+            // 
+            this.redRadioButton.AutoSize = true;
+            this.redRadioButton.Location = new System.Drawing.Point(39, 26);
+            this.redRadioButton.Name = "redRadioButton";
+            this.redRadioButton.Size = new System.Drawing.Size(45, 17);
+            this.redRadioButton.TabIndex = 0;
+            this.redRadioButton.TabStop = true;
+            this.redRadioButton.Text = "Red";
+            this.redRadioButton.UseVisualStyleBackColor = true;
+            this.redRadioButton.CheckedChanged += new System.EventHandler(this.redRadioButton_CheckedChanged);
+            // 
+            // greenRadioButton
+            // 
+            this.greenRadioButton.AutoSize = true;
+            this.greenRadioButton.Location = new System.Drawing.Point(39, 109);
+            this.greenRadioButton.Name = "greenRadioButton";
+            this.greenRadioButton.Size = new System.Drawing.Size(54, 17);
+            this.greenRadioButton.TabIndex = 0;
+            this.greenRadioButton.TabStop = true;
+            this.greenRadioButton.Text = "Green";
+            this.greenRadioButton.UseVisualStyleBackColor = true;
+            this.greenRadioButton.CheckedChanged += new System.EventHandler(this.greenRadioButton_CheckedChanged);
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(145, 26);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(94, 100);
+            this.panel.TabIndex = 1;
+            // 
+            // ColorChangeUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.redRadioButton);
+            this.Controls.Add(this.greenRadioButton);
+            this.Controls.Add(this.blueRadioButton);
+            this.Name = "ColorChangeUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color Change";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton blueRadioButton;
+        private System.Windows.Forms.RadioButton redRadioButton;
+        private System.Windows.Forms.RadioButton greenRadioButton;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ ColorChangeApplicatin/ColorChange/ColorChangeUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorChange
+{
+    public partial class ColorChangeUI : Form
+    {
+        public ColorChangeUI()
+        {
+            InitializeComponent();
+        }
+        
+        private void blueRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (blueRadioButton.Checked)
+            {
+                panel.BackColor = Color.Blue;
+            }
+            else panel.BackColor = Color.Black;
+        }
+
+        private void redRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (redRadioButton.Checked)
+            {
+                panel.BackColor = Color.Red;
+            }
+            else panel.BackColor = Color.Black;
+        }
+
+        private void greenRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (greenRadioButton.Checked)
+            {
+                panel.BackColor = Color.Green;
+            }
+            else panel.BackColor = Color.Black;
+        }
+    }
+}
